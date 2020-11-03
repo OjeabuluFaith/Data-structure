b6e61e2521e22e511e1adb0317de64cba5865327
faith ojeabulu <faithojeabulu@gmail.com>
2020-11-03 22:30:29 +0100

    queue

--- queue.py
+++ queue.py
@@ -16,21 +16,24 @@ implemented using first-in first-out(FIFO)
 class Queue:
     def __init__(self):
         self.item = []
-        
-        
-#check if the queue is empty 
-      
+
+
+# check if the queue is empty
+
     def IsEmpty(self):
         return self.items == []
-    
-#add element  to the queue at the first spot in the queue   
-    def Enqueue(self,item):
-        self.items.insert(0,item)
-        
-#remove element form the queue 
+
+# add element  to the queue at the first spot in the queue
+
+    def Enqueue(self, item):
+        self.items.insert(0, item)
+
+# remove element form the queue
+
     def Dequeue(self):
         return self.items.pop()
-    
-#confirms the size of the queue    
+
+# confirms the size of the queue
+
     def Size(self):
         return len(self.items)
